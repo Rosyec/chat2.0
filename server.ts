4a9c37e342cbfc26a7be2af18c3a25a6687cf57e
Rosyec Parrado <chechomens@gmail.com>
2025-06-17 13:19:56 -0500

    feat: adding socket server and nodemon

---
+++ server.ts
@@ -0,0 +1,29 @@
+import next from "next";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const PORT = process.env.PORT || 3000;
+
+app.prepare().then(() => {
+  const server = createServer((req, res) => handle(req, res));
+  const io = new Server(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+  });
+  io.on("connection", (socket) => {
+    console.log("SOCKET CONECTADO: ", socket.id);
+    socket.on("message", (msg) => {
+      io.emit("message", msg);
+    });
+  });
+
+  server.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+  });
+});
